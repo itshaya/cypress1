78bac6f9a6e33a289d7c49036ad6b4b92df01097
MOSTAFA <201129@ppu.edu.ps>
2025-04-24 15:21:20 +0300

    install some dep and config cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   e2e: {
-    specPattern: "**/*.feature",
+    baseUrl: "https://product-manager-1903f.web.app",
+    specPattern: "**/*.feature", // 👈 important
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
       return config;
     },
   },
