ceb8b099c0c12a86705823435a69c6473bcc12e6
MOSTAFA <201129@ppu.edu.ps>
2025-04-27 12:59:25 +0300

    rewrite actions as class based

--- cypress/e2e/login/pageObjects/actions.ts
+++ cypress/e2e/login/pageObjects/actions.ts
@@ -1,15 +1,19 @@
-export const fillLoginFields = (username: string, password: string) => {
-    cy.get('#username').clear().then($input => {
-        if (username) cy.wrap($input).type(username);
-    });
+ export class LoginActions {
 
-    cy.get('#password').clear().then($input => {
-        if (password) cy.wrap($input).type(password);
-    });
-};
+    static fillLoginFields(username: string, password: string): void {
+        cy.get('#username').clear().then($input => {
+            if (username) cy.wrap($input).type(username);
+        });
 
-export const clickLoginButton = () => {
-    cy.contains('Login').click();
+        cy.get('#password').clear().then($input => {
+            if (password) cy.wrap($input).type(password);
+        });
+    }
+
+    static clickLoginButton(): void {
+        cy.contains('Login').click();
+    }
+
 }
 
 
--- cypress/e2e/login/tests/loginTest/login.ts
+++ cypress/e2e/login/tests/loginTest/login.ts
@@ -1,6 +1,6 @@
 import { emptyUserFields, invalidUser, validUser } from "../../fixtures/data";
-import { fillLoginFields, clickLoginButton } from "../../pageObjects/actions";
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { LoginActions } from "../../pageObjects/actions";
 
 
 Given("I open the login page", () => {
@@ -9,11 +9,11 @@ Given("I open the login page", () => {
 
 // successfully login 
 When("I enter valid credentials", () => {
-    fillLoginFields(validUser.username, validUser.password);
+    LoginActions.fillLoginFields(validUser.username, validUser.password);
 });
 
 When("I click the login button", () => {
-    clickLoginButton();
+    LoginActions.clickLoginButton();
 })
 
 Then("I should be redirected to the categories page", () => {
@@ -23,7 +23,7 @@ Then("I should be redirected to the categories page", () => {
 // login with invalid credentials 
 
 When('I fill fields with invalid credentials', () => {
-    fillLoginFields(invalidUser.username, invalidUser.password);
+    LoginActions.fillLoginFields(invalidUser.username, invalidUser.password);
 });
 
 Then('The login should fail and I should not be redirected to the categories page', () => {
@@ -33,7 +33,7 @@ Then('The login should fail and I should not be redirected to the categories pag
 // login with empty fields  
 
 When('I try to leave the login fields empty', () => {
-    fillLoginFields(emptyUserFields.password, emptyUserFields.password);
+    LoginActions.fillLoginFields(emptyUserFields.password, emptyUserFields.password);
 })
 
 Then('The login should fail and see validation error messages', () => {
