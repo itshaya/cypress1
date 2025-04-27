249347752f5ed372d96f94e6214ff94d35044073
MOSTAFA <201129@ppu.edu.ps>
2025-04-27 13:37:38 +0300

    change fature writing

--- cypress/e2e/login/tests/loginTest/login.ts
+++ cypress/e2e/login/tests/loginTest/login.ts
@@ -4,26 +4,26 @@ import { LoginActions } from "../../pageObjects/actions";
 import { LoginAssertions } from "../../pageObjects/assertions";
 
 
-Given("I open the login page", () => {
+Given("User open the login page", () => {
     cy.visit("/sign-in");
 });
 
 // successfully login 
-When("I enter valid credentials", () => {
+When("User enter valid credentials", () => {
     LoginActions.fillLoginFields(validUser.username, validUser.password);
 });
 
-When("I click the login button", () => {
+When("click the login button", () => {
     LoginActions.clickLoginButton();
 })
 
-Then("I should be redirected to the categories page", () => {
+Then("User should be redirected to the categories page", () => {
     LoginAssertions.verifyRedirectionToCategoriesPage();
 });
 
 // login with invalid credentials 
 
-When('I fill fields with invalid credentials', () => {
+When('User fill fields with invalid credentials', () => {
     LoginActions.fillLoginFields(invalidUser.username, invalidUser.password);
 });
 
@@ -33,7 +33,7 @@ Then('The login should fail and I should not be redirected to the categories pag
 
 // login with empty fields  
 
-When('I try to leave the login fields empty', () => {
+When('User try to leave the login fields empty', () => {
     LoginActions.fillLoginFields(emptyUserFields.password, emptyUserFields.password);
 })
 
