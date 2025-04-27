4a0b0ad97914aa19e23545f64313631e62a3ddf8
MOSTAFA <201129@ppu.edu.ps>
2025-04-27 13:50:22 +0300

    add custom command for reusability

--- cypress/e2e/login/pageObjects/actions.ts
+++ cypress/e2e/login/pageObjects/actions.ts
@@ -1,13 +1,8 @@
  export class LoginActions {
 
     static fillLoginFields(username: string, password: string): void {
-        cy.get('#username').clear().then($input => {
-            if (username) cy.wrap($input).type(username);
-        });
-
-        cy.get('#password').clear().then($input => {
-            if (password) cy.wrap($input).type(password);
-        });
+        cy.clearAndType('#username',username);     
+        cy.clearAndType('#password',password);     
     }
 
     static clickLoginButton(): void {
