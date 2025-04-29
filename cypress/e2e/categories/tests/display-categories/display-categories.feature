e1d90aa692a637810d4cdae68e623c0bedd122e3
MOSTAFA <201129@ppu.edu.ps>
2025-04-29 19:29:04 +0300

    add test for second and third scenarios

--- cypress/e2e/categories/tests/display-categories/display-categories.feature
+++ cypress/e2e/categories/tests/display-categories/display-categories.feature
@@ -1,9 +1,22 @@
 Feature: Display Categories in a Paginated Table
 
+  @TC_001
   Scenario: Verify Displaying Categories with correct Timestamps format
     Given The user is logged in
     And navigates to the Categories page
     When the list of categories is loaded
     Then each category row should display a "Created At" timestamp in "YYYY-MM-DD HH:MM:SS" format
 
-  Scenario:
+  @TC_002
+  Scenario: :Verify that  user can navigate between pages using Next/Previous buttons
+    Given The user is logged in
+    And navigates to the Categories page
+    When user clicks the "Next" or "Previous" button
+    Then The system should navigate to the selected page and update the displayed categories
+
+  @TC_003
+  Scenario: Verify that user can control the number of items displayed per page
+    Given The user is logged in
+    And navigates to the Categories page
+    When The user selects a specific number of items per page
+    Then Then The system should display the selected number of categories per page
--- cypress/e2e/categories/tests/display-categories/display-categories.ts
+++ cypress/e2e/categories/tests/display-categories/display-categories.ts
@@ -18,3 +18,37 @@ When('the list of categories is loaded', () => {
 Then('each category row should display a "Created At" timestamp in "YYYY-MM-DD HH:MM:SS" format', () => {
     CategoriesAssertions.verifyCreatedAtFormat();
 })
+
+var pageNum: number;
+When('user clicks the "Next" or "Previous" button', () => {
+    cy.get('button').contains('Next').as('nextButton');
+    cy.get('.table-controls__actions').children('p').as('prevPageNum');
+    cy.get('@prevPageNum')
+        .invoke('text')
+        .then((text) => {
+            pageNum = Number(text.trim())
+        });
+
+    cy.get('@nextButton').click();
+})
+
+Then('The system should navigate to the selected page and update the displayed categories', () => {
+    cy.get('.table-controls__actions')
+        .children('p')
+        .invoke('text')
+        .then((text) => {
+            const updatedPageNum = Number(text.trim());
+            expect(updatedPageNum).to.equal(pageNum + 1);
+        });
+})
+var itemNum: number;
+When('The user selects a specific number of items per page', () => {
+    cy.get('#query').select('10');
+    itemNum = 10;
+})
+
+Then('Then The system should display the selected number of categories per page', () => {
+    cy.get('.table-body')
+        .children()
+        .should('have.length', itemNum);
+})
